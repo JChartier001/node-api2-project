839c96091a7805c2a08785806d18d8a8e8b34ee2
JChartier001 <JChartier001@gmail.com>
2019-12-03 18:34:46 -0600

    working on put error

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -19,31 +19,111 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
 // const text= req.params.text;
 // const content = req.params.contents;
-
-    posts.insert(req.body)
-    .then(message => {
-        if(title && contents){
-            res.status(201).json(message)
-                        
-        } else{
-            res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
-        }
+const postData = req.body;
+if(postData.title || postData.contents === null){
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })                    
+}   else{
+posts.insert(postData)
+    .then(postData => {
+            res.status(201).json(postData);    
     })
     .catch(error => {
         console.log(error)
         res.status(500).json({ error: "There was an error while saving the post to the database" })
-    })
-})
+    });}
+});
+
 
 router.post('/:id/comments', (req, res) => {
-    posts.insertComment(req.body)
+    const blog = posts.findById((blogId) => blogId.id === id);
+    const id = req.params.id;
+    const comment = req.body;
+
+    if(!blog){
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else if(!comment.text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    }
+    else{
+    posts.insertComment(comment)
     .then(comment => {
+        
+            res.status(201).json(comment);
         
     })
     .catch(error =>{
         console.log(error);
         res.status(500).json({ error: "There was an error while saving the comment to the database" })
-    })
+    });}
+});
+
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    posts.findById(id)
+        .then(post => {
+            if (post.length) {
+                posts.findPostComments(id)
+                    .then(comments => {
+                        res.status(200).json(comments);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            } else {
+                res.status(404).json({error: 'The post with the specified ID does not exist.'})
+            }})
+        .catch(error => {
+            res.status(500).json({error: 'The comments information could not be retrieved'})
+        })      
 })
+
+
+router.delete('/:id', (req, res) => {  
+    const { id } = req.params;
+    posts.findById(id)
+        .then(post => {
+            if (post.length) {
+                posts.remove(id)
+                    .then(deleted => {
+                        res.status(200).json(id);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            } else {
+                res.status(404).json({error: 'The post with the specified ID does not exist.'})
+            }})
+        .catch(error => {
+            res.status(500).json({error: 'The comments information could not be retrieved'})
+        })      
+});
+
+router.put('/:id', (req, res) => {  
+    const { id } = req.params;
+    const changes = req.body;
+    console.log(changes)
+    posts.findById(id)
+        .then(post => {            
+            if (!changes.title || !changes.contents) {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+                } else if (posts.length === 0){
+                    res.status(404).json({error: 'The post with the specified ID does not exist.'})
+                } else {
+                posts.update(id, changes)
+                console.log(changes)
+                .then(post => {
+                        res.status(200).json(changes);
+                    })
+                .catch(error => {
+                    console.log(error);
+                    })
+            }
+            })
+        .catch(error => {
+            res.status(500).json({error: 'The comments information could not be retrieved'})
+        })      
+});
+
+
 
 module.exports = router;
